0d920a523dd123893afaf4f0c85b2fe48b034e60
Hafis20 <hafisvh2002@gmail.com>
2024-05-31 15:45:58 +0530

    environment file bug fixed

---
+++ Client/src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    menteeslotURL: 'https://mentorcode.onrender.com/api/menteeslot',
+    menteeURL: "https://mentorcode.onrender.com/api/mentee",
+    mentorslotURL: "https://mentorcode.onrender.com/api/mentorslot",
+    mentorURL: "https://mentorcode.onrender.com/api/mentor",
+    walletURL: "https://mentorcode.onrender.com/api/wallet",
+    paymentURL: "https://mentorcode.onrender.com/api/payment",
+    adminURL: "https://mentorcode.onrender.com/api/admin"
+};
