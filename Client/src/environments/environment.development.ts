97f615d67ac2f501e8eb978d2792918a5730d60b
Hafis20 <hafisvh2002@gmail.com>
2024-06-03 14:59:59 +0530

    password hide/show build

--- Client/src/environments/environment.development.ts
+++ Client/src/environments/environment.development.ts
@@ -1,20 +1,20 @@
-// export const environment = {
-//     menteeslotURL: 'https://mentorcode.onrender.com/api/menteeslot',
-//     menteeURL: "https://mentorcode.onrender.com/api/mentee",
-//     mentorslotURL: "https://mentorcode.onrender.com/api/mentorslot",
-//     mentorURL: "https://mentorcode.onrender.com/api/mentor",
-//     walletURL: "https://mentorcode.onrender.com/api/wallet",
-//     paymentURL: "https://mentorcode.onrender.com/api/payment",
-//     adminURL: "https://mentorcode.onrender.com/api/admin"
-// };
-
-
 export const environment = {
-    menteeslotURL: 'http://localhost:7000/api/menteeslot',
-    menteeURL: "http://localhost:7000/api/mentee",
-    mentorslotURL: "http://localhost:7000/api/mentorslot",
-    mentorURL: "http://localhost:7000/api/mentor",
-    walletURL: "http://localhost:7000/api/wallet",
-    paymentURL: "http://localhost:7000/api/payment",
-    adminURL: "http://localhost:7000/api/admin"
+    menteeslotURL: 'https://mentorcode.onrender.com/api/menteeslot',
+    menteeURL: "https://mentorcode.onrender.com/api/mentee",
+    mentorslotURL: "https://mentorcode.onrender.com/api/mentorslot",
+    mentorURL: "https://mentorcode.onrender.com/api/mentor",
+    walletURL: "https://mentorcode.onrender.com/api/wallet",
+    paymentURL: "https://mentorcode.onrender.com/api/payment",
+    adminURL: "https://mentorcode.onrender.com/api/admin"
 };
+
+
+// export const environment = {
+//     menteeslotURL: 'http://localhost:7000/api/menteeslot',
+//     menteeURL: "http://localhost:7000/api/mentee",
+//     mentorslotURL: "http://localhost:7000/api/mentorslot",
+//     mentorURL: "http://localhost:7000/api/mentor",
+//     walletURL: "http://localhost:7000/api/wallet",
+//     paymentURL: "http://localhost:7000/api/payment",
+//     adminURL: "http://localhost:7000/api/admin"
+// };
